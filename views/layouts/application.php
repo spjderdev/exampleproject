cd6be55513fb8e4bc9f123698ddba9f1b93f8fd6
spjderdev <tuandattobi@gmail.com>
2024-07-24 19:06:42 +0700

    cleanup(layout): remove navbar inclusion and Flowbite script

--- views/layouts/application.php
+++ views/layouts/application.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./public/assets/css/styles.css">
+    <link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <title>Document</title>
 </head>
 <body>
-    <?php include './components/navbar.php' ?>
     <?= @$content ?>
-    <script src="https://unpkg.com/flowbite@1.5.1/dist/flowbite.js"></script>
 </body>
 </html>
