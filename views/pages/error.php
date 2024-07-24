8f50c3311a0237710d6d5ed463e84f90b737b8f4
spjderdev <tuandattobi@gmail.com>
2024-07-25 00:51:16 +0700

    feat: add error pages

--- views/pages/error.php
+++ views/pages/error.php
@@ -1,3 +1,11 @@
-<?php 
-
-echo "Something went wrong";
+<section class="flex items-center h-screen p-16 bg-gray-50 dark:bg-gray-700">
+    <div class="container flex flex-col items-center ">
+        <div class="flex flex-col gap-6 max-w-md text-center">
+            <h2 class="font-extrabold text-9xl text-redCustom dark:text-gray-100">
+                <span class="sr-only">Error</span><span></span>404
+            </h2>
+            <p class="text-2xl md:text-3xl dark:text-gray-300">Sorry, we couldn't find this page.</p>
+            <a href="index.php?controller=pages&action=home" class="px-8 py-4 text-xl font-semibold rounded bg-redCustom text-gray-50 hover:text-gray-200">Back to home</a>
+        </div>
+    </div>
+</section>
