b446de0fb56e3051c9fd71e122bda88f4861f24c
spjderdev <tuandattobi@gmail.com>
2024-07-25 00:50:47 +0700

    refactor: remove unnecessary php and change template

--- views/pages/home.php
+++ views/pages/home.php
@@ -6,14 +6,20 @@
     <title>Document</title>
 </head>
 <body>
-        <?php foreach($products as $product): ?>
-        <ul>
-            <li><?= $product->id?></li>
-            <li><?= $product->title?></li>
-            <li><?= $product->description?></li>
-            <li><?= $product->price?></li>
-            <li><img src="<?= $product->images?>" alt=""></li>
-        </ul>
-        <?php endforeach; ?>
+        <?php include './components/navbar.php' ?>
+ 
+        <div class="flex">
+            <div class="relative w-full">
+                <img class="w-full h-[600px] object-cover" src="./public/images/bannerseed.jpg" alt="">
+                <div class="absolute top-[100px] left-[200px] w-[500px]">
+                    <h1 class="text-8xl text-white font-big">NEW VANDAL ARRIVED</h1>
+                    <p class="text-xl text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ipsa cupiditate placeat iusto quo nostrum nobis? Commodi quasi hic repudiandae doloribus inventore repellendus, beatae vel! Facilis corporis iure ipsam facere?</p>
+                </div>
+                <div class="absolute top-0 right-0">
+                    <img class="w-full h-[600px] object-cover"src="./public/images/seedingagent.png" alt="">
+                </div>
+
+            </div>
+        </div>
 </body>
 </html>
