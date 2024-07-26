f9059a89b8b382b4d09ccc3874375fe960de1e4f
spjderdev <tuandattobi@gmail.com>
2024-07-26 14:41:48 +0700

    feat: add category routes, dynamic controller and action

--- routes.php
+++ routes.php
@@ -3,9 +3,11 @@
 $controllers = [
     'pages' => ['home', 'error'],
     'account' => ['signin', 'signup', 'register', 'login', 'logout'],
-    'dashboard' => ['home', 'add']
+    'dashboard' => ['home', 'add'],
+    'category' => ['list', 'edit']
 ];
 
+$id = isset($_GET['id']) ? $_GET['id'] : null;
 if (!array_key_exists($controller, $controllers) || !in_array($action, $controllers[$controller])) {
     $controller = 'pages';
     $action = 'error';
@@ -14,5 +16,23 @@ if (!array_key_exists($controller, $controllers) || !in_array($action, $controll
 include_once 'controllers/' . ucfirst($controller) . 'Controller.php';
 
 $class = ucfirst($controller) . 'Controller';
-$controller = new $class;
-$controller -> $action();
+
+if(class_exists($class)) {
+    $controller = new $class;
+    if(method_exists($controller, $action)) {
+        if($action === 'edit' &&  $id !== null) {
+            $controller -> $action($id);
+        }
+        else {
+            $controller -> $action();
+        }
+    }
+    else {
+        $controller = 'pages';
+        $action = 'error';
+    }
+}
+else {
+    $controller = 'pages';
+    $action = 'error';
+}
