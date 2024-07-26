309c60ec1c8fa4c0f4f85b24fa5a73def759dcae
spjderdev <tuandattobi@gmail.com>
2024-07-26 20:05:29 +0700

    feat: add edit/delete products by id

--- models/Product.php
+++ models/Product.php
@@ -2,6 +2,7 @@
 
 
 class Product {
+    public $id;
     public $title;
     public $description;
     public $price;
@@ -9,7 +10,8 @@ class Product {
     public $category_id;
     public $category_name;
 
-    function __construct($category_id, $title, $description, $price, $images, $category_name = null) {
+    function __construct($id,$category_id, $title, $description, $price, $images, $category_name = null) {
+        $this -> id = $id;
         $this -> title = $title;
         $this -> description = $description;
         $this -> price = $price;
@@ -26,12 +28,24 @@ class Product {
                   JOIN categories c ON p.category_id = c.id');
 
         foreach($req->fetchAll() as $item) {
-            $list[] = new Product($item['category_id'], $item['title'], $item['description'], $item['price'], $item['images'], $item['category_name']);
+            $list[] = new Product($item['id'],$item['category_id'], $item['title'], $item['description'], $item['price'], $item['images'], $item['category_name']);
         }
         return $list;
     }
 
-    function AddProduct($title, $description, $price, $category_id, $images) {
+    static function getProductById($id) {
+        $db = Database::getInstance();
+        $query = "SELECT p.id, p.title, p.description, p.price, p.images, p.category_id, c.name as category_name
+                  FROM products p
+                  JOIN categories c ON p.category_id = c.id
+                  WHERE p.id = :id";
+        $req = $db -> prepare($query);
+        $req -> bindParam(':id', $id);
+        $req -> execute();
+        return $req->fetch(PDO::FETCH_ASSOC); 
+    }
+
+    static function addProduct($title, $description, $price, $category_id, $images) {
         $db = Database::getInstance();
         $query = "INSERT INTO products (title, description, price, category_id, images) VALUES (:title, :description, :price, :category_id, :images)";
         $req = $db -> prepare($query);
@@ -47,5 +61,44 @@ class Product {
         else {
             throw new Exception("Error add products ") . $req -> getMessage();
         }
+    }
+
+    static function deleteProduct($id) {
+        $db = Database::getInstance();
+        $query = "DELETE FROM products where id = :id";
+        $req = $db -> prepare($query);
+        $req -> bindParam(':id', $id);
+        if($req -> execute()) {
+            return true;
+        }
+        else {
+            throw new Exception("Error when deleting products ") . $req ->getMessage();
+        }
+    }
+
+    static function editProduct($id, $title, $description, $price, $category_id, $images) {
+        $db = Database::getInstance();
+        if(isset($images)) {
+            $query = "UPDATE products SET title = :title, description = :description, price = :price, category_id = :category_id, images = :images WHERE id = :id";
+        }
+        else {
+            $query = "UPDATE products SET title = :title, description = :description, price = :price, category_id = :category_id WHERE id = :id";
+        }
+        $req = $db->prepare($query);
+        $req -> bindParam(':id', $id);
+        $req -> bindParam(':title', $title);
+        $req -> bindParam(':description', $description);
+        $req -> bindParam(':price', $price);
+        $req -> bindParam(':category_id', $category_id);
+        if(isset($images)) {
+            $req -> bindParam(':images', $images);
+        }
+        if($req -> execute()) {
+            return true;
+        }
+        else {
+            throw new Exception("Error when edit products ") . $req -> getMessage();
+        }
+
     }
 }
