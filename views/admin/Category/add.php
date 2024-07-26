7e0a0a85b0f60ff78e4079b20a258a109db5221e
spjderdev <tuandattobi@gmail.com>
2024-07-26 16:55:10 +0700

    feat: add category pages

--- views/admin/Category/add.php
+++ views/admin/Category/add.php
@@ -1,1 +1,10 @@
-<h1 class="text-white">Add Category</h1>
+
+<?php include './components/sidebar.php'?>
+<div class="ml-[20rem] mt-[100px]">
+    <h1 class="text-white font-bold font-poppins text-[20px]">Create new category</h1>
+    <form method="POST">
+        <label for="name">Name of category</label>
+        <input type="text" name="name">
+        <button type="submit" name="add">Create</button>
+    </form>
+</div>
