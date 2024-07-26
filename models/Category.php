d27eb12e0c11b3d7e73f6211a8d995f0fed9e455
spjderdev <tuandattobi@gmail.com>
2024-07-26 15:25:55 +0700

    feat: add Category models and related methods

---
+++ models/Category.php
@@ -0,0 +1,64 @@
+<?php 
+
+require_once 'Product.php';
+
+class Category {
+    public $id;
+    public $name;
+
+    function __construct($id, $name) {
+        $this -> id = $id;
+        $this -> name = $name;
+    }
+
+    static function getAllCategory() {
+        $list = [];
+        $db = Database::getInstance();
+        $req = $db -> query("SELECT * FROM categories");
+
+        foreach($req->fetchAll() as $item) {
+            $list[] = new Category($item['id'], $item['name']);
+        }
+        return $list;
+    }
+
+    static function countProduct() {
+        $db = Database::getInstance();
+        try {
+            $query = "SELECT c.id, c.name, COUNT(p.id) AS product_count FROM categories c
+            LEFT JOIN products p ON c.id = p.category_id
+            GROUP BY c.id, c.name;";
+            $req = $db -> query($query);
+            return $req->fetchAll(PDO::FETCH_ASSOC);
+        } catch(PDOException $e) {
+            echo 'Something went wrong: ' . $e->getMessage();
+        }
+    }
+
+    static function getCategoryById($id) {
+        $db = Database::getInstance();
+        try {
+            $query = "SELECT * FROM categories WHERE id = :id";
+            $req = $db -> prepare($query);
+            $req -> bindParam(':id', $id, PDO::PARAM_INT);
+            $req -> execute();
+
+            return $req->fetchAll(PDO::FETCH_ASSOC);   
+        } catch(PDOException $e) {
+            echo 'Something went wrong: ' . $e->getMessage();
+        }
+    }
+
+    static function editCategory($id, $name) {
+        $db = Database::getInstance();
+        try {
+            $query = "UPDATE categories SET name = :name WHERE id = :id";
+            $req = $db -> prepare($query);
+            $req -> bindParam(':id', $id);
+            $req -> bindParam(':name', $name);
+            return $req -> execute();
+        } catch(PDOException $e) {
+            echo 'Something went wrong: ' . $e -> getMessage();
+        }
+    }
+}
