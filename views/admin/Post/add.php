9dbd743cf409482129ba064b9b1fac7828b4b337
spjderdev <tuandattobi@gmail.com>
2024-07-26 20:12:09 +0700

    feat: create post pages

--- views/admin/Post/add.php
+++ views/admin/Post/add.php
@@ -1,21 +1,20 @@
 
     <div class="flex flex-col">
-        <form class="flex flex-col" action="index.php?controller=dashboard&action=add" enctype="multipart/form-data" method="POST">
-            <label for="Title">Title</label>
+        <form class="flex flex-col" enctype="multipart/form-data" method="POST">
+            <label class="text-white text-bold" for="Title">Title</label>
             <input  class="border" type="text" name="title" placeholder="Title">
-            <label for="Description">Description</label>
+            <label class="text-white text-bold" for="Description">Description</label>
             <input class="border" name="description" type="text">
-            <label for="Price">Price</label>
+            <label class="text-white text-bold" for="Price">Price</label>
             <input class="border" name="price" type="number">
-            <label for="Category">Category</label>
+            <label class="text-white text-bold" for="Category">Category</label>
             <select name="category_id" id="">
-                <option value="1">Vandal</option>
-                <option value="2">Phantom</option>
-                <option value="3">Sheriff</option>
-                <option value="4">Melee</option>
+                <?php foreach($categories as $category): ?>
+                    <option value="<?= $category-> id ?>"><?= $category->name ?></option>
+                <?php endforeach;?>
             </select>
             <label for="images">Images</label>
             <input type="file" name="images">
-            <button type="submit" name="add">Add</button>
+            <button class="text-white text-bold" type="submit" name="add">Add</button>
         </form>
     </div>
