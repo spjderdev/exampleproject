e314ed8f70a442ec8dad60c69da5a51b050398bd
spjderdev <tuandattobi@gmail.com>
2024-08-03 12:50:05 +0700

    feat: edit/addProduct will have video parameters

--- models/Product.php
+++ models/Product.php
@@ -9,8 +9,9 @@ class Product {
     public $images;
     public $category_id;
     public $category_name;
+    public $video;
 
-    function __construct($id,$category_id, $title, $description, $price, $images, $category_name = null) {
+    function __construct($id,$category_id, $title, $description, $price, $images, $video,$category_name = null) {
         $this -> id = $id;
         $this -> title = $title;
         $this -> description = $description;
@@ -18,24 +19,25 @@ class Product {
         $this -> images = $images;
         $this -> category_id = $category_id;
         $this -> category_name = $category_name;
+        $this -> video = $video;
     }
 
     static function getAllProduct() {
         $list = [];
         $db = Database::getInstance();
-        $req = $db -> query('SELECT p.id, p.title, p.description, p.price, p.images, p.category_id, c.name as category_name
+        $req = $db -> query('SELECT p.id, p.title, p.description, p.price, p.images, p.category_id, p.video, c.name as category_name
                   FROM products p
                   JOIN categories c ON p.category_id = c.id');
 
         foreach($req->fetchAll() as $item) {
-            $list[] = new Product($item['id'],$item['category_id'], $item['title'], $item['description'], $item['price'], $item['images'], $item['category_name']);
+            $list[] = new Product($item['id'],$item['category_id'], $item['title'], $item['description'], $item['price'], $item['images'], $item['video'], $item['category_name']);
         }
         return $list;
     }
 
     static function getProductById($id) {
         $db = Database::getInstance();
-        $query = "SELECT p.id, p.title, p.description, p.price, p.images, p.category_id, c.name as category_name
+        $query = "SELECT p.id, p.title, p.description, p.price, p.images, p.category_id, p.video, c.name as category_name
                   FROM products p
                   JOIN categories c ON p.category_id = c.id
                   WHERE p.id = :id";
@@ -45,15 +47,16 @@ class Product {
         return $req->fetch(PDO::FETCH_ASSOC); 
     }
 
-    static function addProduct($title, $description, $price, $category_id, $images) {
+    static function addProduct($title, $description, $price, $category_id, $images, $video) {
         $db = Database::getInstance();
-        $query = "INSERT INTO products (title, description, price, category_id, images) VALUES (:title, :description, :price, :category_id, :images)";
+        $query = "INSERT INTO products (title, description, price, category_id, images, video) VALUES (:title, :description, :price, :category_id, :images, :video)";
         $req = $db -> prepare($query);
         $req -> bindParam(':title', $title);
         $req -> bindParam(':description', $description);
         $req -> bindParam(':price', $price);
         $req -> bindParam(':category_id', $category_id);
         $req -> bindParam(':images', $images);
+        $req -> bindParam(':video', $video);
 
         if($req -> execute()) {
             return true;
@@ -76,23 +79,35 @@ class Product {
         }
     }
 
-    static function editProduct($id, $title, $description, $price, $category_id, $images) {
+    static function editProduct($id, $title, $description, $price, $category_id, $images = null, $video = null) {
         $db = Database::getInstance();
-        if(isset($images)) {
-            $query = "UPDATE products SET title = :title, description = :description, price = :price, category_id = :category_id, images = :images WHERE id = :id";
+        $query = "UPDATE products SET title = :title, description = :description, price = :price, category_id = :category_id";
+
+        if($images !== null) {
+            $query .= ", images = :images";
         }
-        else {
-            $query = "UPDATE products SET title = :title, description = :description, price = :price, category_id = :category_id WHERE id = :id";
+
+        if($video !== null) {
+            $query .= ", video = :video";
         }
+
+        $query .= " WHERE id = :id";
+
         $req = $db->prepare($query);
+
         $req -> bindParam(':id', $id);
         $req -> bindParam(':title', $title);
         $req -> bindParam(':description', $description);
         $req -> bindParam(':price', $price);
         $req -> bindParam(':category_id', $category_id);
-        if(isset($images)) {
+        
+        if($images !== null) {
             $req -> bindParam(':images', $images);
         }
+        if($video !== null) {
+            $req -> bindParam(':video', $video);
+        }
+
         if($req -> execute()) {
             return true;
         }
