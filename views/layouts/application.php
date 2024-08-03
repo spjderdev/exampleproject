4d9c68d7cdb0411635cea8ff825ef15ee8b9e32f
spjderdev <tuandattobi@gmail.com>
2024-08-03 12:57:44 +0700

    style:  add animation on scroll and font awesome

--- views/layouts/application.php
+++ views/layouts/application.php
@@ -6,9 +6,22 @@
     <link rel="stylesheet" href="./public/assets/css/styles.css">
     <link rel="stylesheet"
     href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css"
+    />
+    <link 
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css";
+    />
     <title>Valorant Store</title>
 </head>
 <body class="bg-backgroundColor">
     <?= @$content ?>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
 </body>
 </html>
